44b4d15483dad48215c562c37e7ecaf64c5169c0
Aritra Nanda <nandaaritra53@gmail.com>
2026-06-09 23:27:24 +0530

    Improve chatbot responses

--- Chatbot.py
+++ Chatbot.py
@@ -1,4 +1,10 @@
-name = input("What is your name? ")
+user = input("You: ")
 
-print("Hello,", name)
-print("Welcome to my chatbot!")
+if "hello" in user.lower():
+    print("Bot: Hello!")
+elif "name" in user.lower():
+    print("Bot: My name is Aritra Bot")
+elif "bye" in user.lower():
+    print("Bot: Goodbye!")
+else:
+    print("Bot: Sorry, I don't understand.")
